e2cbd9a6b7ea63f7a08195a09248d54a3e1c045d
ats.2727-ls10@ezweb.ne.jp <ats.2727-ls10@ezweb.ne.jp>
2018-04-18 21:32:01 +0900

    大まかな内容作成

---
+++ Assets/script/cameraconfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraconfig : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.FindWithTag("Player");
+        Vector3 cpos = transform.position;
+        Vector3 ppos = player.transform.position;
+        cpos.x = ppos.x;
+        cpos.y = ppos.y;
+        cpos.z = -10;
+        transform.position = cpos;
+	}
+}
---
+++ Assets/script/playercon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercon : MonoBehaviour {
+    private float time = 60;
+	// Use this for initialization
+	void Start () {
+   
+	}
+   
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        float a;
+        int se;
+         a = Time.realtimeSinceStartup;  //ゲームが始まってからのリアルタイム時間数
+        se = (int)a;//小数点の削除     
+
+    
+        if (Input.GetKey(KeyCode.F))
+          {
+              Time.timeScale = 0;//停止
+          }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pp");
+            rigidbody.AddForce(new Vector2(0, 700));
+        }
+
+	}
+}
