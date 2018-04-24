a4ee5d7c62a4c72af44cc3f952a2bf6b6950adca
雑食娘 <zassyokukko686@gmail.com>
2018-04-24 11:16:35 +0900

    score Zako

---
+++ Assets/script/ScoreScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+    private int scores = playercon.score;
+    Text ScoreText;
+	// Use this for initialization
+	void Start () {
+        ScoreText = GetComponent < Text > ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scores = playercon.score;
+        ScoreText.text ="Score:"+ scores.ToString();
+	}
+}
